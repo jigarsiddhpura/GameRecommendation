cbe5046d5e995424b3f219b4701684b810d7a4b8
Jigar <95428432+jigarsiddhpura@users.noreply.github.com>
2024-05-19 21:15:26 +0530

    feat : Evaluate @ real-time

--- functions.py
+++ functions.py
@@ -1,3 +1,7 @@
+from llama_index.core.evaluation import (  
+    FaithfulnessEvaluator, RelevancyEvaluator
+) 
+from langchain_anthropic import AnthropicLLM
 import uuid
 
 def delta_index(documents):
@@ -14,6 +18,19 @@ def delta_index(documents):
 
     return uniqueIds, uniqueDocuments
 
+def evaluate(critic_llm, query, response):        
+    critic_llm = AnthropicLLM(  
+        model='claude-2.1',
+        max_tokens=250  
+    )  
 
+    faithfulness_evaluator = FaithfulnessEvaluator(llm=critic_llm)  
+    relevancy_evaluator = RelevancyEvaluator(llm=critic_llm)
+
+    faithfulness_eval_result = faithfulness_evaluator.evaluate_response(response=response)
+
+    relevancy_eval_result = relevancy_evaluator.evaluate_response(query=query, response=response)
+    
+    return str(faithfulness_eval_result.score), str(faithfulness_eval_result.passing), str(relevancy_eval_result.score), str(relevancy_eval_result.passing)
 
 
